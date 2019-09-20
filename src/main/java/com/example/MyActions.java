d5db12ab6efc797a38266be928cbbc73c37eb6c6
imoh <imohchard@gmail.com>
2019-09-20 10:54:22 +0100

    changes made due to springboot implementation and controller addition

--- src/main/java/com/example/MyActions.java
+++ src/main/java/com/example/MyActions.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyActions {
     public static void main(String[] args) {
-
         SpringApplication.run(MyActions.class, args);
     }
 }
